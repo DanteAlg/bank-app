298455f3706ee4b827567ee55494b3a692ae6f6e
Dante Miranda <dante.alighierimds@gmail.com>
2020-01-20 20:20:45 -0300

    Remove corneal files

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -5,9 +5,4 @@ def app
 end
 
 describe ApplicationController do
-  it "responds with a welcome message" do
-    get '/'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Sinatra Template!")
-  end
 end
