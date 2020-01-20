de6e64ca05ac69a7c9fcc523e047bab7662f97c3
Dante Miranda <dante.alighierimds@gmail.com>
2020-01-20 20:52:57 -0300

    Add balance by sum financial transactions to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,11 @@
 class Account < ActiveRecord::Base
+	has_many :financial_transactions
+
   belongs_to :user
 
   validates_presence_of :number, :user
+
+  def balance
+    financial_transactions.sum(:amount)
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -15,4 +15,24 @@ describe Account do
 		include_examples 'must be present', 'user'
 		include_examples 'must be present', 'number'
 	end
+
+	describe '#balance' do
+		let(:account) { FactoryBot.build(:account) }
+		let!(:transaction_1) do
+			FactoryBot.create(:financial_transaction, account: account, amount: 1000,
+				kind: FinancialTransaction::KINDS[:credit])
+		end
+		let!(:transaction_2) do
+			FactoryBot.create(:financial_transaction, account: account, amount: 3000,
+				kind: FinancialTransaction::KINDS[:credit])
+		end
+		let!(:transaction_3) do
+			FactoryBot.create(:financial_transaction, account: account, amount: -2050,
+				kind: FinancialTransaction::KINDS[:debit])
+		end
+
+		it 'sum account financial transactions' do
+			expect(account.balance).to eq(1950)
+		end
+	end
 end
